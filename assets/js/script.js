0492f0deaef1f8fe31c43b6102b73562d6cf0ba6
DonnaThompson7 <130340823+DonnaThompson7@users.noreply.github.com>
2023-05-16 14:01:51 -0400

    created elements for calendar time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@
 // in the html.
 
 $(function () {
+    var largeContainerEl = $('.container-lg');
     // TODO: Add a listener for click events on the save button. 
     var saveButtonEl = $('.saveBtn');
     saveButtonEl.on('click', saveSchedule);
@@ -16,23 +17,51 @@ $(function () {
         console.log('called saveSchedule');
     }
 
-    //
+    // Use Day.js to to get the current hour in 24-hour time:
+    var currentHour = dayjs().hour();
+    console.log("Current hour = " + currentHour);
+    
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? 
-    
-    // Use Day.js to to get the current hour in 24-hour time:
-    var currentHour = dayjs().hour();
-    console.log("Current hour = " + currentHour);
 
+    //loop thru array of standard business hours 8am-5pm
+    for (var i = 8; i < 18; i++) {
+        var timeBlockDivEl;
+        var index = i;
+        console.log('entered for loop to create time blocks and index = ' + index + " and currentHour = " + currentHour);
+        if (index < currentHour) {
+            console.log('creating PAST time blocks');
+            //create time block and add class = "past"
+            timeBlockDivEl = $('<div id="hour-9" class="row time-block past">');
+        } else if (index === currentHour) {
+            //create time block and add class = "present"
+            console.log('creating PRESENT time blocks');
+            timeBlockDivEl = $('<div id="hour-10" class="row time-block present">');
+        } else {
+            //create time block and add class = "future"
+            console.log('creating FUTURE time blocks');
+            timeBlockDivEl = $('<div id="hour-11" class="row time-block future">');
+        }
+        largeContainerEl.append(timeBlockDivEl);
+        var timeBlockLabelEl = $('<div class="col-2 col-md-1 hour text-center py-3"></div>');
+        timeBlockLabelEl.text(index);
+        var timeBlockTextEl = $('<textarea class="col-8 col-md-10 description" rows="3"> </textarea>');
+        var timeBlockSaveBtnEl = $('<button class="btn saveBtn col-2 col-md-1" aria-label="save"></button>');
+        var timeBlockInputEl = $('<i class="fas fa-save" aria-hidden="true"></i>');
+    
+        timeBlockDivEl.append(timeBlockLabelEl);
+        timeBlockDivEl.append(timeBlockTextEl);
+        timeBlockDivEl.append(timeBlockSaveBtnEl);
+        timeBlockSaveBtnEl.append(timeBlockInputEl);
+
+    }
 
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
 
-
-    
     // TODO: Add code to display the current date in the header of the page.
     var currentDate = dayjs();
     $('#current-date-time').text(currentDate.format('MMM DD, YYYY'));
